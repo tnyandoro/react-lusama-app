d995a8bf2b2bc7f8869880a69b12aff115fa9155
Tendai <tendai1@hotmail.com>
2021-08-27 23:52:44 +0200

    create the main section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <div className="grid-container">
           <header>
-            
+            <a href="/">Lusama Shop</a>
           </header>
+          <main>
+          </main>
         </div>
-      </header>
     </div>
   );
 }
