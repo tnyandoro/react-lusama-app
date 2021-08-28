936bac091c21c102f4b42d10e69f51f7cfe30fd6
Tendai <tendai1@hotmail.com>
2021-08-28 21:39:26 +0200

    Add product img styling

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 
+
 export default class Products extends Component {
   render() {
     return (
       <div>
         <ul className="products">
-          {this.props.products.map((product) => (
-            <li key={product._id}>
-              <div className="product">
+              {this.props.products.map((product) => (
+                <li key={product._id}>
+                  <div className="product">
                 <a href={"# + product._id"}>
                   <img src={product.image} alt={product.title}></img>
                   <p>{product.title}</p>
                 </a>
-                <div className="price-price">{product.price}</div>
+                <div className="product-price">{product.price}</div>
                 <button className="button primary">Add to cart</button>
               </div>
             </li>
