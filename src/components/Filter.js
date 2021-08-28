2171f85d99f20b61d0dc75a0c22f73ef5221839e
Tendai <tendai1@hotmail.com>
2021-08-28 23:12:22 +0200

    Add the filter sizes options

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Filter extends Component {
   render() {
     return (
       <div>
         <div className="filter-result">{this.props.count}</div>
-        <div className="filter-sort"> Order <select></select></div>
-        <div className="filter-size"></div>
+        <div className="filter-sort">
+          {" "}
+          Order <select></select>
+        </div>
+        <div className="filter-size">
+          Filter
+          <select>
+            <option value="">All</option>
+            <option value="">All</option>
+            <option value="">All</option>
+            <option value="">All</option>
+            <option value="">All</option>
+            <option value="">All</option>
+            <option value="">All</option>
+          </select>
+        </div>
       </div>
-    )
+    );
   }
 }
-
