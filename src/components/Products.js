ea6a8f49c84498c83b38e25ad0b938b40d06afc9
Tendai <tendai1@hotmail.com>
2021-08-28 14:07:14 +0200

    Add procoduct details for the product component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Products extends Component {
   render() {
     return (
       <div>
-        
+        <ul className="products">
+          {this.props.products.map((product) => (
+            <li key={product._id}>
+              <div className="product">
+                <a href="#">
+                  <img src={product.image} alt="product image"></img>
+                  <p>{product.title}</p>
+                </a>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
